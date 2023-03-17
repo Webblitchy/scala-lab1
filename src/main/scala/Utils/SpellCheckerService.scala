02c090401766cdf202bd86511eb548225ffa1565
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2023-03-17 10:13:59 +0100

    add source

--- src/main/scala/Utils/SpellCheckerService.scala
+++ src/main/scala/Utils/SpellCheckerService.scala
@@ -27,40 +27,32 @@ end SpellCheckerService
 class SpellCheckerImpl(val dictionary: Map[String, String]) extends SpellCheckerService:
   // DONE - Part 1 Step 2
   def stringDistance(s1: String, s2: String): Int = 
-    val dist = Array.tabulate(s2.length + 1, s1.length + 1){ (j, i) => 
-      if j == 0 then
-        i 
-      else if i == 0 then
-        j 
-      else 
-        0
-    }
+    // inspiré de https://rosettacode.org/wiki/Levenshtein_distance#Translated_Wikipedia_algorithm
 
-    @inline
-    def minimum(i: Int*): Int = i.min
+    val dist = Array.tabulate(s2.length + 1, s1.length + 1){ 
+      (j, i) => 
+        if j == 0 then
+          i 
+        else if i == 0 then
+          j 
+        else 
+          0
+    }
     
     for 
       j <- dist.indices.tail
       i <- dist(0).indices.tail
     do dist(j)(i) =
-        if (s2(j - 1) == s1(i - 1)) then
+        if s2(j - 1) == s1(i - 1) then
           dist(j - 1)(i - 1)
         else 
-          minimum(dist(j - 1)(i) + 1, dist(j)(i - 1) + 1, dist(j - 1)(i - 1) + 1)
+          List(
+            dist(j - 1)(i) + 1, dist(j)(i - 1) + 1,
+            dist(j - 1)(i - 1) + 1
+          ).min
     
     dist(s2.length)(s1.length)
 
-    // Ancienne version
-    // (s1,s2) match 
-    //   case ("",s2) => s2.length
-    //   case (s1,"") => s1.length
-    //   case (s1,s2) => 
-    //     val cost = if s1.last == s2.last then 0 else 1
-    //     List(
-    //       stringDistance(s1.init, s2) + 1,
-    //       stringDistance(s1, s2.init) + 1,
-    //       stringDistance(s1.init, s2.init) + cost
-    //     ).min
 
   // DONE - Part 1 Step 2
   def getClosestWordInDictionary(misspelledWord: String): String = 
