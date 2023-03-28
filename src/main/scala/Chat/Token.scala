32ea31e518af42587e68a6ff97355064b297b154
Webblitchy <55515713+Webblitchy@users.noreply.github.com>
2023-03-28 17:33:48 +0200

    part 2 step 1

--- src/main/scala/Chat/Token.scala
+++ src/main/scala/Chat/Token.scala
@@ -3,19 +3,40 @@ package Chat
 enum Token:
   case // Terms
        // TODO - Part 2 Step 1
+
+       // Interjections
        BONJOUR, 
-       JE,
        SVP,
+       
+       // Questions
+       QUEL,
+       COMBIEN,
+       
+       // Articles
+       JE,
+       ME,
+       LE,
+       MON,
+       DE,
+
+       // Adjectives
        ASSOIFFE,
        AFFAME,
        // Actions
        ETRE,
        VOULOIR,
+       COUTER,
+       COMMANDER,
+       CONNAITRE,
+       APPELER,
        // Logic Operators
        ET,
        OU,
        // Products
        PRODUIT,
+       MARQUE,
+       PRIX,
+       SOLDE,
        // Util
        PSEUDO,
        NUM,
